b9cb4d83a8c1d2418b1cb9f9ab1456df67b5a25a
abdallahalhasan09 <ahhmi9p@gmail.com>
2022-08-18 21:03:44 +0300

    first commit

---
+++ js/java.js
@@ -0,0 +1,101 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  // dots[slideIndex-1].className += " active";
+}
+
+
+let nums = document.querySelectorAll(".awesome-boxese  .num");
+let section = document.getElementById("count");
+let started = false;  
+
+
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop -500) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+
+
+let slid1 = document.getElementById("slide-one")
+let slid2 = document.getElementById("slide-tow")
+let slid3 = document.getElementById("slide-three")
+ 
+
+let section_slide = document.getElementById('slideshow-container');
+
+
+if (window.scrollY >= section_slide.offsetTop - 50) {
+
+  console.log('hi');
+
+    slid1.style.opacity = slid1.dataset.opacity;
+    slid2.style.opacity = slid2.dataset.opacity;
+    slid3.style.opacity = slid3.dataset.opacity;
+
+  // imgs.forEach((img) => {
+  //   img.style.opacity = img.dataset.opacity;
+  // });
+  // divs.forEach((div) => {
+  //   div.style.opacity = div.dataset.opacity;
+  // });
+}
+
+
+
+jQuery(document).ready(function($){
+  var isLateralNavAnimating = false;
+  
+  //open/close lateral navigation
+  $('.cd-nav-trigger').on('click', function(event){
+    event.preventDefault();
+    //stop if nav animation is running 
+    if( !isLateralNavAnimating ) {
+      if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+      
+      $('body').toggleClass('navigation-is-open');
+      $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+        //animation is over
+        isLateralNavAnimating = false;
+      });
+    }
+  });
+});
---
+++ js/jq.js
@@ -0,0 +1,110 @@
+$(function () {
+  $("#nav-menu-btn").click(function () {
+    $("#list-hidden").slideToggle("fast");
+  });
+  $("#home-ul").click(function () {
+    $("#list-hidden-home").slideToggle("fast");
+  });
+  $("#menu-ul").click(function () {
+    $("#list-hidden-menu").slideToggle("fast");
+  });
+  $("#pages-ul").click(function () {
+    $("#list-hidden-pages").slideToggle("fast");
+  });
+  $("#shop-ul").click(function () {
+    $("#list-hidden-shop").slideToggle("fast");
+  });
+  $("#blog-ul").click(function () {
+    $("#list-hidden-blog").slideToggle("fast");
+  });
+
+  $( "#blog" ).click(function() {
+    $( "#mega-menu-blog" ).slideToggle();
+  });
+  $( "#shop" ).click(function() {
+    $( "#mega-menu-shop" ).slideToggle();
+  });
+  $( "#pages" ).click(function() {
+    $( "#mega-menu-pages" ).slideToggle();
+  });
+  $( "#p-menu" ).click(function() {
+    $( "#mega-menu-menu" ).slideToggle();
+  });
+  $( "#home" ).click(function() {
+    $( "#mega-menu-home" ).slideToggle();
+  });
+});
+
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000);
+
+
+  
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+
+
+  
+let add = document.getElementById('increment');
+let remove = document.getElementById('decrement');
+let number = document.getElementById('number');
+
+let counter =0;
+
+add.addEventListener('click',function(){
+  counter +=1
+  number.innerHTML = counter;
+});
+
+remove.addEventListener('click',function(){
+  counter -=1
+  number.innerHTML = counter;
+});
+
+let add1 = document.getElementById('increment1');
+let remove1 = document.getElementById('decrement1');
+let number1 = document.getElementById('number1');
+
+let counter1 =0;
+
+add1.addEventListener('click',function(){
+  counter1 +=1
+  number1.innerHTML = counter1;
+});
+
+remove1.addEventListener('click',function(){
+  counter1 -=1
+  number1.innerHTML = counter1;
+});
+
+let add2 = document.getElementById('increment2');
+let remove2 = document.getElementById('decrement2');
+let number2 = document.getElementById('number2');
+
+let counter2 =0;
+
+add2.addEventListener('click',function(){
+  counter2 +=1
+  number2.innerHTML = counter2;
+});
+
+remove2.addEventListener('click',function(){
+  counter2 -=1
+  number2.innerHTML = counter2;
+});
+
+
+
+
